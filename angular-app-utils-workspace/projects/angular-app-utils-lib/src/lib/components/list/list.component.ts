1b1b13865221004c5028572a7408fdcb0ca67d4c
Lorenzo Bolognesi <>
2020-08-27 18:26:37 +0200

    doppio click in liste

--- angular-app-utils-workspace/projects/angular-app-utils-lib/src/lib/components/list/list.component.ts
+++ angular-app-utils-workspace/projects/angular-app-utils-lib/src/lib/components/list/list.component.ts
@@ -26,6 +26,7 @@ export abstract class ListComponent<T, LoginInfo> extends GenericComponent<T, Lo
   @Input() dataSource: T[] | MatTableDataSource<T>;
   selectedElement: T;
   @Output() onSelectElement = new EventEmitter<T>();
+  @Output() onDoubleSelectElement = new EventEmitter<T>();
   protected dataSub: Subscription;
 
   protected refreshAll: boolean = true; //booleano per decidere se ricaricare tutta la lista al momento di un aggiornamento di un item
@@ -34,6 +35,7 @@ export abstract class ListComponent<T, LoginInfo> extends GenericComponent<T, Lo
   protected detailComponentType: Type<any>;
   protected openDetailOnClick: boolean = true;
   protected detailDialogLoadFromServer: boolean = false;
+  protected inSelectorDialog: boolean = false;
 
   constructor(protected httpClient: HttpClient,
     protected dataRefreshService: DataRefreshService,
@@ -41,29 +43,29 @@ export abstract class ListComponent<T, LoginInfo> extends GenericComponent<T, Lo
     protected router: Router,
     authService: AuthenticationService<LoginInfo>,
     public dialog: MatDialog) {
-      super(httpClient, userMessageService, authService);     
-      this.currentPath = this.router.url;
-      this.sub.add(this.router.events.subscribe((val) => {
-        this.onRouteChanged(val);
-      }));
-      /*if(this.dataSource == null && this.loadDataOnLoad){
-        //se non è stato valorizzato dataSource tramite @Input, si chiama loadListData
-        this.isLoadingResults = true;
-      }*/
+    super(httpClient, userMessageService, authService);
+    this.currentPath = this.router.url;
+    this.sub.add(this.router.events.subscribe((val) => {
+      this.onRouteChanged(val);
+    }));
+    /*if(this.dataSource == null && this.loadDataOnLoad){
+      //se non è stato valorizzato dataSource tramite @Input, si chiama loadListData
+      this.isLoadingResults = true;
+    }*/
   }
 
   protected onRouteChanged(val: import("@angular/router").Event) {
-    if(val instanceof NavigationEnd && val.url.endsWith(this.currentPath)){
+    if (val instanceof NavigationEnd && val.url.endsWith(this.currentPath)) {
       this.onNavigationEnded(val);
-    }  
+    }
   }
 
   protected onNavigationEnded(val: NavigationEnd) {
     this.selectedElement = null;
   }
-  
+
   ngOnInit(): void {
-    if(this.dataSource == null && this.loadDataOnLoad){
+    if (this.dataSource == null && this.loadDataOnLoad) {
       //se non è stato valorizzato dataSource tramite @Input, si chiama loadListData
       this.loadListData();
     }
@@ -73,23 +75,23 @@ export abstract class ListComponent<T, LoginInfo> extends GenericComponent<T, Lo
     //this.setupPaginatorAndSort();
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     super.ngOnDestroy();
-    if(this.dataSub){
+    if (this.dataSub) {
       this.dataSub.unsubscribe();
     }
   }
 
   setupPaginatorAndSort() {
-    if(this.dataSub){
+    if (this.dataSub) {
       this.dataSub.unsubscribe();
     }
     this.dataSub = new Subscription();
-    if(this.paginator){
-      if(this.sort){
+    if (this.paginator) {
+      if (this.sort) {
         this.dataSub.add(this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0));// If the user changes the sort order, reset back to the first page.
         this.dataSub.add(merge(this.sort.sortChange, this.paginator.page).subscribe(() => this.loadListData()));
-      }else{
+      } else {
         this.dataSub.add(this.paginator.page.subscribe(() => this.loadListData()));
       }
     }
@@ -97,12 +99,12 @@ export abstract class ListComponent<T, LoginInfo> extends GenericComponent<T, Lo
 
   @HostListener('window:storage', ['$event'])
   onStorageChange(ev: StorageEvent) {
-    if(ev.key && ev.key.startsWith(DATA_REFRESH_SERVICE_TAG)){
-       /*console.log('onStorageChange', ev.key);
-       console.log('oldValue', ev.oldValue);        
-       console.log('newValue', ev.newValue);  */
-       this.refreshFromService(JSON.parse(ev.newValue) )
-    }     
+    if (ev.key && ev.key.startsWith(DATA_REFRESH_SERVICE_TAG)) {
+      /*console.log('onStorageChange', ev.key);
+      console.log('oldValue', ev.oldValue);        
+      console.log('newValue', ev.newValue);  */
+      this.refreshFromService(JSON.parse(ev.newValue))
+    }
   }
 
   /**
@@ -112,11 +114,11 @@ export abstract class ListComponent<T, LoginInfo> extends GenericComponent<T, Lo
    * @param res DatRefreshItem<T> oggetto contenente le info da valutare per il refresh. 
    */
   refreshFromService(res: DataRefreshItem) {
-    if(res && res.IdentifierName === this.LIST_NAME){
-      if(this.refreshAll || (res.ElementUpdatedId == null && res.ElementUpdated == null)){
+    if (res && res.IdentifierName === this.LIST_NAME) {
+      if (this.refreshAll || (res.ElementUpdatedId == null && res.ElementUpdated == null)) {
         //si ricarica tutta la lista se refreshAll è true o se non abbiamo info sul detail aggiornato
         this.loadListData();
-      }else{
+      } else {
         //si aggiorna il solo elemento
         this.refreshItemList(res.Action, res.ElementUpdatedId, res.ElementUpdated);
       }
@@ -139,7 +141,7 @@ export abstract class ListComponent<T, LoginInfo> extends GenericComponent<T, Lo
     this.setupPaginatorAndSort();
   }
 
-  setDataSourceAttributes() {}//utile per grid-list.component
+  setDataSourceAttributes() { }//utile per grid-list.component
 
   /**
    * metodo per aggiornare un elemento nella lista
@@ -150,43 +152,43 @@ export abstract class ListComponent<T, LoginInfo> extends GenericComponent<T, Lo
    * @param id id del detail da aggiungere/aggiornare/eliminare
    * @param el il detail completo
    */
-  refreshItemList(action: ApiActionsType, id: any, el?: T){
-    if(el == null){
-      if(id){
+  refreshItemList(action: ApiActionsType, id: any, el?: T) {
+    if (el == null) {
+      if (id) {
         //se abbiamo id
-        if(action == ApiActionsType.DeleteAction){
+        if (action == ApiActionsType.DeleteAction) {
           //se è il caso di una delete, si toglie il rigo dalla lista/grid
-          if(this.dataSource instanceof MatTableDataSource){
+          if (this.dataSource instanceof MatTableDataSource) {
             //caso della grid
             this.deleteItemRow(id);
-          }else{
+          } else {
             //caso lista generica
             this.dataSource = this.dataSource.filter((item: T) => {
               return this.idExtractor(item) !== id;
             })
-          }          
-        }else {
+          }
+        } else {
           //altrimenti si richiede il detail al server
           this.loadSingleRowById(id, action);
         }
-      }else{
+      } else {
         //se mancano el e id, si logga l'impossibilità di esecuzione
         this.listError = true;
-        console.log("ListComponent", "Impossibile ricaricare il dettaglio nella lista. Manca id");          
+        console.log("ListComponent", "Impossibile ricaricare il dettaglio nella lista. Manca id");
       }
-    }else{
+    } else {
       //se esiste el, si manipola direttamente il dataSource
-      if(this.dataSource instanceof MatTableDataSource){
+      if (this.dataSource instanceof MatTableDataSource) {
         //gestione in caso di grid
         this.refreshItemRow(action, id, el);
-      }else{
+      } else {
         switch (action) {
           case ApiActionsType.AddAction:
             this.dataSource.push(el);
             break;
           case ApiActionsType.UpdateAction:
             this.dataSource = this.dataSource.map((item: T) => {
-              if(this.idExtractor(item) === this.idExtractor(el)){
+              if (this.idExtractor(item) === this.idExtractor(el)) {
                 return el;
               }
               return item;
@@ -196,12 +198,12 @@ export abstract class ListComponent<T, LoginInfo> extends GenericComponent<T, Lo
             this.dataSource = this.dataSource.filter((item: T) => {
               return this.idExtractor(item) !== this.idExtractor(el);
             })
-            break;      
+            break;
           default:
             break;
         }
       }
-      
+
     }
   }
   /**
@@ -218,21 +220,21 @@ export abstract class ListComponent<T, LoginInfo> extends GenericComponent<T, Lo
    * @param id id del detail da aggiungere/aggiornare/eliminare
    * @param el il detail completo 
    */
-  refreshItemRow(action: ApiActionsType, id: any, el: T){
+  refreshItemRow(action: ApiActionsType, id: any, el: T) {
     //TODO implementato in GridListComponent
   }
-  
+
   loadSingleRowById(id: number | string, action: ApiActionsType = ApiActionsType.AddAction) {
     this.isLoadingResults = true;
     this.listError = false;
     this.sub.add(this.apiDatasource.getElement(id).subscribe((data) => {
       this.isLoadingResults = false;
-      if(data != null){
+      if (data != null) {
         this.refreshItemList(action, id, data);
       }
-    },(error) => {
+    }, (error) => {
       this.listError = true;
-      console.error(error);      
+      console.error(error);
     }));
   }
 
@@ -240,62 +242,48 @@ export abstract class ListComponent<T, LoginInfo> extends GenericComponent<T, Lo
    * carica la lista dal server. Volendo si può passare una funzione di callback da eseguire dopo onItemLoaded()
    * @param callback funzione di callback opzionale
    */
-  loadListData(callback?: () => void ) {
+  loadListData(callback?: () => void) {
     this.isLoadingResults = true;
     this.listError = false;
     const params: HttpParams = this.prepareLoadParameters();
     this.sub.add(this.apiDatasource.getFilteredElements(params).subscribe((data: T[] | ApiPaginatorListResponse<T>) => {
-      if(data['totalCount']){
+      if (data['totalCount']) {
         this.resultsLength = data['totalCount'];
       }
-      if(data['items']){
+      if (data['items']) {
         this.onListLoaded(data['items']);
-      }else{
+      } else {
         this.onListLoaded(data as T[]);
       }
-      if(callback != null){
+      if (callback != null) {
         callback();
       }
       this.isLoadingResults = false;
-    },(error) => {
+    }, (error) => {
       console.error("ListComponent: errore nel caricamento dati", error);
       this.isLoadingResults = false;
       this.listError = true;
     }));
   }
-  
+
   protected prepareLoadParameters(): HttpParams {
     let params = new HttpParams();
-    if(this.sort && this.sort.active){
+    if (this.sort && this.sort.active) {
       params = params.set("sort", `${this.sort.active}`);
       params = params.set("order", `${this.sort.direction}`)
     }
-    if(this.paginator){
+    if (this.paginator) {
       params = params.set("page", `${this.paginator.pageIndex + 1}`);
       params = params.set("pageSize", `${this.paginator.pageSize}`);
     }
-    if(this.dataSource && this.dataSource.filter){
+    if (this.dataSource && this.dataSource.filter) {
       params = params.set("filter", `${this.dataSource.filter}`);
     }
     return params;
   }
 
-  protected onListLoaded(data: T[]){
-    this.dataSource = data;    
-  }
-
-  onItemSelected(item: T){
-    if(this.selectedElement == null || this.idExtractor(this.selectedElement) != this.idExtractor(item)){
-      this.selectedElement = item;
-    }else{
-      //altrimenti si deseleziona
-      this.selectedElement = null;
-    }
-    this.onSelectElement.emit(this.selectedElement);
-    if(this.openDetailOnClick && this.selectedElement != null){
-      this.openDetail(item);
-    }
-    
+  protected onListLoaded(data: T[]) {
+    this.dataSource = data;
   }
 
   prepareRoute(outlet: RouterOutlet) {
@@ -306,20 +294,20 @@ export abstract class ListComponent<T, LoginInfo> extends GenericComponent<T, Lo
    * Metodo chiamato per filtrare i risultati
    * @param event evento generato contenente l'input con il valore con cui eseguire il filtro
    */
-  applyFilter(event: Event): void{
-    
+  applyFilter(event: Event): void {
+
   }
 
   /**
    * metodo per aprire un detail per inserire un nuovo elemento
    */
-  addNewClicked(){
-    if(this.isAuthorizedToModify()){
+  addNewClicked() {
+    if (this.isAuthorizedToModify()) {
       this.selectedElement = null;
       this.openDetail(null);
     }
   }
-  
+
   openDetail(el: T) {
     if (this.subscribeRoute) {
       this.router.navigate([this.currentPath, el ? this.apiDatasource.apiIdExtractor(el) : 'new']);
@@ -332,14 +320,14 @@ export abstract class ListComponent<T, LoginInfo> extends GenericComponent<T, Lo
    * Apre dialog con detail component al suo interno
    * @param el element da aprire nel dialog
    */
-  protected openDetailDialog(el: T = {} as T){
-    const dialogData: DetailDialogData<T> = this.getDetailDialogData(el);    
+  protected openDetailDialog(el: T = {} as T) {
+    const dialogData: DetailDialogData<T> = this.getDetailDialogData(el);
     let dialogRef = this.dialog.open(DetailDialogComponent, {
       data: dialogData
     });
-    this.sub.add(dialogRef.afterClosed().subscribe( (result: T) => { 
-      this.loadListData();  
-    })); 
+    this.sub.add(dialogRef.afterClosed().subscribe((result: T) => {
+      this.loadListData();
+    }));
   }
 
   /**
@@ -348,7 +336,7 @@ export abstract class ListComponent<T, LoginInfo> extends GenericComponent<T, Lo
    * @param metaData Object che può contenere vari attributi da valorizzare per il
    */
   protected getDetailDialogData(el: T = {} as T): DetailDialogData<T> {
-    if(this.detailComponentType == null){
+    if (this.detailComponentType == null) {
       throw Error("ListComponent: detailComponentType non instanziato");
     }
     const dialogData: DetailDialogData<T> = {
@@ -365,7 +353,67 @@ export abstract class ListComponent<T, LoginInfo> extends GenericComponent<T, Lo
   protected getDetailMetaData(el: T): Object {
     return {
       subscribeRoute: false,
-    } 
+    }
   }
+
+
+  //************** GESTIONE CLICK E DOPPIO CLICK **************
+  protected timer: any;
+  protected delay = 200;
+  protected prevent = false;
+
+  /**
+   * chiamato alla selezione di un item
+   * @param item item selezionato
+   */
+  onItemSelected(item: T) {
+    if (this.selectedElement == null || this.idExtractor(this.selectedElement) != this.idExtractor(item)) {
+      this.selectedElement = item;
+    } else {
+      //altrimenti si deseleziona
+      this.selectedElement = null;
+    }
+    this.onSelectElement.emit(this.selectedElement);
+    if (this.openDetailOnClick && this.selectedElement != null) {
+      this.openDetail(item);
+    }
+  }
+
+  /**
+   * chiamato alla doppia selezione di un item
+   * NOTA: da sovrascrivere per stabilire un diverso comportamento dal semplice click
+   * @param item item selezionato
+   */
+  onItemDoubleClick(item: T) {
+    this.onDoubleSelectElement.emit(item);
+  }
+
+  /**
+   * chiamato al click di una riga. Gestisce l'eventuale doppio click. Può essere annullato dal doppio click
+   * NOTA: se non è previsto di usare il doppio click, si può usare direttamente la funzione onItemSelected
+   * @param item item selezionato
+   */
+  onRowClicked(item: T) {
+    let self = this;
+    this.timer = setTimeout(function () {
+      if (!self.prevent) {
+        self.onItemSelected(item);
+      }
+      self.prevent = false;
+    }, this.delay);
+  }
+
+  /**
+   * chiamato al doppio click di una riga. Impedisce l'esecuzione della chiamata del singolo click
+   * @param item item selezionato
+   */
+  onRowDoubleClicked(item: T) {
+    clearTimeout(this.timer);
+    this.prevent = true;
+    this.onItemDoubleClick(item);
+  }
+  // ************** FINE GESTIONE CLICK E DOPPIO CLICK **************
+
+
 
 }

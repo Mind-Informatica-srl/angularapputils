d7214261705b1fd765452cad27d8763a48153136
lorenzoB07 <82948587+lorenzoB07@users.noreply.github.com>
2021-05-17 16:45:25 +0200

    package.json; titolo file csv e pdf di list component; bug form di ricerca

--- angular-app-utils-workspace/projects/angular-app-utils-lib/src/lib/components/html-container-dialog/html-container-dialog.component.ts
+++ angular-app-utils-workspace/projects/angular-app-utils-lib/src/lib/components/html-container-dialog/html-container-dialog.component.ts
@@ -1,27 +1,35 @@
-import { Component, Inject, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {
+  Component,
+  Inject,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+} from "@angular/core";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
 
 export interface HtmlContainerDialogData {
   htmlTitle: string;
   htmlBody: string;
   htmlOkButton: string;
   styleString: string;
+  title?: string;
 }
 
 @Component({
-  selector: 'app-html-container-dialog',
-  templateUrl: './html-container-dialog.component.html',
-  styleUrls: ['./html-container-dialog.component.scss']
+  selector: "app-html-container-dialog",
+  templateUrl: "./html-container-dialog.component.html",
+  styleUrls: ["./html-container-dialog.component.scss"],
 })
 export class HtmlContainerDialogComponent implements AfterViewInit {
-
   printed: boolean = false;
-  @ViewChild('bodyElement') bodyElement: ElementRef;
-  @ViewChild('titleElement') titleElement: ElementRef;
-  @ViewChild('headerContainer') headerContainer: ElementRef;
+  @ViewChild("bodyElement") bodyElement: ElementRef;
+  @ViewChild("titleElement") titleElement: ElementRef;
+  @ViewChild("headerContainer") headerContainer: ElementRef;
 
-  constructor(public dialogRef: MatDialogRef<HtmlContainerDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: HtmlContainerDialogData) {
+  constructor(
+    public dialogRef: MatDialogRef<HtmlContainerDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: HtmlContainerDialogData
+  ) {
     dialogRef.disableClose = true;
   }
 
@@ -32,42 +40,47 @@ export class HtmlContainerDialogComponent implements AfterViewInit {
   }
 
   createStyle(): void {
-    const headerElement = document.createElement('head');
+    const headerElement = document.createElement("head");
     //headerElement.appendChild(document.createTextNode(this.data.styleString));
     headerElement.innerHTML = this.data.styleString;
     this.bodyElement.nativeElement.appendChild(headerElement);
   }
 
   createTitle(): void {
-    const content = document.createElement('div');
+    const content = document.createElement("div");
     content.innerHTML = this.data.htmlTitle;
     this.titleElement.nativeElement.appendChild(content);
   }
 
   createBody(): void {
-    const content = document.createElement('div');
+    const content = document.createElement("div");
     content.innerHTML = this.data.htmlBody;
     this.bodyElement.nativeElement.appendChild(content);
   }
 
   print() {
     var divText = this.bodyElement.nativeElement.outerHTML;
-    var myWindow = window.open('', '', 'height=700,width=1000');
+    var myWindow = window.open("", "", "height=700,width=1000");
     var doc = myWindow.document;
     doc.open();
-    doc.write('<html>');
+    doc.write("<html>");
+    if (this.data.title != null && this.data.title != "") {
+      doc.write("<head>");
+      doc.write("<title>");
+      doc.write(this.data.title);
+      doc.write("</title>");
+      doc.write("</head>");
+    }
     doc.write(divText);
-    doc.write('</html>');
+    doc.write("</html>");
     doc.close();
 
     myWindow.focus();
     setTimeout(function () {
       myWindow.print();
       myWindow.close();
-    }, 500);//necessario  timeout, altrimenti viene pagina vuota
+    }, 500); //necessario  timeout, altrimenti viene pagina vuota
     this.printed = true;
     return true;
-
   }
-
 }
